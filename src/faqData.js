806174f36dca4f46c5d1f2a5d43d0d241901e48f
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2022-03-20 12:57:20 +0530

    Add all faq data into faqData object file

---
+++ src/faqData.js
@@ -0,0 +1,26 @@
+const faqData = [
+  {
+    faq: "How many team members can I invite?",
+    answer: "You can invite up to 2 additional users on the Free plan." +
+       "There is no limit on team members for the Premium plan."
+  },
+  {
+    faq: "What is the maximum file upload size?",
+    answer: "No more than 2GB. All files in your account must fit your allotted storage space."
+  },
+  {
+    faq:"How do I reset my password?",
+    answer: "Click “Forgot password” from the login page or “Change password” from your profile page." +
+    "A reset link will be emailed to you."
+  },
+  {
+    faq:"Can I cancel my subscription?",
+    answer:"Yes! Send us a message and we’ll process your request no questions asked."
+  },
+  {
+    faq:"Do you provide additional support?",
+    answer:"Chat and email support is available 24/7. Phone lines are open during normal business hours."
+  }
+]
+
+export default faqData;
